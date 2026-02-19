84a31421193ef76eb410cf1c266ae9f6f3aa11c6
saloni <salonilathwariya581@gmail.com>
2026-02-19 15:36:21 +0530

    Configure multer memory storage middleware for avatar uploads

---
+++ server/middlewares/upload.middleware.js
@@ -0,0 +1,20 @@
+import multer from 'multer';
+
+const storage = multer.memoryStorage();
+
+const fileFilter = (req, file, callback) => {
+    const allowedTypes = ['image/png', 'image/jpg', "image/gif", "image/webp"];
+
+    if(!allowedTypes.includes(file.mimetype)) {
+        return callback(new Error("File type is not allowed!"), false);
+    } else
+    return callback(null, true);
+}
+
+const limits = {
+    fileSize: 5 * 1024 * 1024,
+}
+
+const upload = multer({ storage, limits, fileFilter });
+
+export default upload;
--- server/routes/visuals.routes.js
+++ server/routes/visuals.routes.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { auth } from "../middlewares/auth.middleware.js";
+import upload from "../middlewares/upload.middleware.js";
 
 const router = express.Router();
 
-router.post("/avatar", auth, uploadUserAvatar);
+router.post("/avatar", auth, upload.single("avatar"), uploadUserAvatar);
 
 export default router;
