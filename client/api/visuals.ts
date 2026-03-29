fb26fd32f89df925b56e6812a7262aca7c3bcc8d
saloni <salonilathwariya581@gmail.com>
2026-03-29 11:16:58 +0530

    fix(avatar): remove incorrect Content-Type header from uploadAvatar API call.

--- client/api/visuals.ts
+++ client/api/visuals.ts
@@ -2,9 +2,6 @@ import axios from "axios";
 
 const api = axios.create({
     baseURL: "http://localhost:3000/api/visuals",
-    headers: {
-        "Content-Type": "application/json",
-    },
     withCredentials: true
 });
 
